4390e2c3c34c083c81ba647888d835fa6f7a09ac
iwamoto-ma <iwamoto-ma@gnavi.co.jp>
2019-03-11 15:11:38 +0900

    [add], puppeteer / ava - e2e

---
+++ puppeteer/README.md
@@ -0,0 +1,17 @@
+## puppeteer / ava - e2e
+
+### Recommended Env
+- node: `^10.15.1`
+- npm: `6.4.1`
+
+### Installation
+
+```
+$ npm i
+```
+
+### Usage
+
+```
+$ npm test
+```
---
+++ puppeteer/test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+import * as fs from "fs";
+
+const puppeteer = require('puppeteer')
+const test = require('ava')
+
+test.serial('check form searchResult', async (t) => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  page.setViewport({width: 1200, height: 800})
+
+  let searchResult
+
+  try {
+    await page.goto('https://www.gnavi.co.jp/dressing/')
+    await page.type('input[name="fw"]', '恵比寿')
+    await page.click('.search__btn')
+    await page.waitForNavigation()
+
+    searchResult = await page.evaluate(() => {
+      return Promise.resolve(document.querySelector('.contents-header__title').textContent)
+    })
+  } catch (e) {
+    t.fail(e)
+  }
+
+  const testFileDir = './test_file/'
+  const fileName = `puppeteer${new Date().getTime()}.png`
+
+  if (!fs.existsSync(testFileDir)) {
+    fs.mkdirSync(testFileDir)
+  }
+
+  await page.screenshot({ path: `${testFileDir}${fileName}` })
+
+  browser.close()
+  console.log(searchResult)
+  t.truthy(searchResult)
+})
